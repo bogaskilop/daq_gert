af233ce1edbf32dad12a33236022298a0996feb2
Fred Brooks <nsaspook@nsaspook.com>
2012-10-14 19:43:12 -0700

    source update

--- RPi_PIC/SlaveM2.c
+++ RPi_PIC/SlaveM2.c
@@ -9,15 +9,28 @@
  * have been interconnected in the standard way for a PIC18F8722 chip
  */
 
+//#define P25K22
+#define P8722
+
+#include "xlcd.h"
+//#include <p18cxxx.h>
+#ifdef P8722
+#include <p18f8722.h>
 #pragma	config OSC = HSPLL
 #pragma config WDT = OFF
 #pragma config BOREN = OFF
 #pragma config STVREN = ON
 #pragma config LVP=OFF
+#endif
 
-#include "xlcd.h"
-#include <p18cxxx.h>
-#include <p18f8722.h>
+#ifdef P25K22
+#include <p18f25k22.h>
+#pragma	config	FOSC = INTIO7
+#pragma config	PLLCFG=ON
+#pragma config	WDTEN = OFF
+#pragma config	CCP2MX = PORTC1, PBADEN = OFF, T3CMX = PORTC0
+#endif
+
 #include <spi.h>
 #include <adc.h>
 #include <delays.h>
@@ -27,16 +40,14 @@
 #include <math.h>
 #include <GenericTypeDefs.h>
 
-
-#define SPI_SS_PIN	PORTAbits.RA5
-#define SPI_SS		LATAbits.LATA5
-
 /* bit 7 high for command
- * bits 6..4 command code
- * bits 4..0 port address
+ * bit 7 low  for config data in CMD_DUMMY per uC type
+ * bits 6 config bit code always 1
+ * bit	5 0=ADC ref VSS 1=ADC rec FVR=2.048
+ * bit  4 0=10bit adc, 1=12bit adc
+ * bits 3..0 port address
  *
  */
-
 
 #define CMD_ADC_GO	0b10000000
 #define CMD_ADC_GO_0	0b10000000
@@ -44,7 +55,16 @@
 #define CMD_ADC_DONE	0b11110000
 #define CMD_ADC_DATAL	0b10010000
 #define CMD_ADC_DATAH	0b10100000
-#define CMD_DUMMY	0b01111111
+
+#define CMD_DUMMY_CFG	0b01000000
+#ifdef P8722
+#define CMD_DUMMY	0b01001100	/* 12 channels VDD */
+#define NUM_AI_CHAN	12
+#endif
+#ifdef P25K22
+#define CMD_DUMMY	0b01101001	/* 9 channels 2.048 but only 8 are ADC */
+#define NUM_AI_CHAN     9
+#endif
 
 /* LCD defines */
 #define LCD_L           4                       // lines
@@ -108,6 +128,7 @@ const rom char *build_date = __DATE__, *build_time = __TIME__;
 volatile uint8_t data_in1, data_in2, adc_buffer_ptr = 0,
 	adc_channel = 0, dsi = 0, SPI_DATA = FALSE, ADC_DATA = FALSE,
 	REMOTE_LINK = FALSE, REMOTE_DATA_DONE = FALSE, LOW_BITS = FALSE;
+
 volatile uint32_t adc_count = 0, adc_error_count = 0;
 volatile uint16_t adc_buffer[64] = {0}, adc_data_recv = 0;
 #pragma udata gpr13
@@ -137,12 +158,15 @@ void InterruptVectorHigh(void)
 
 void InterruptHandlerHigh(void)
 {
-    static uint8_t  channel=0;
+    static uint8_t channel = 0;
+
     if (PIR1bits.ADIF) { // ADC conversion complete flag
 	PIR1bits.ADIF = LOW;
 	adc_count++; // just keep count
 	adc_buffer[channel] = ADRES;
+#ifdef P8722
 	LATEbits.LATE0 = !LATEbits.LATE0;
+#endif
 	SSP1BUF = (uint8_t) adc_buffer[channel]; // stuff with lower 8 bits
 	ADC_DATA = TRUE;
     }
@@ -150,11 +174,22 @@ void InterruptHandlerHigh(void)
     if (PIR1bits.SSP1IF) { // SPI port #1 SLAVE receiver
 	PIR1bits.SSP1IF = LOW;
 	data_in1 = SSP1BUF;
+#ifdef P8722
 	LATEbits.LATE1 = !LATEbits.LATE1;
-
+#endif
+#ifdef P25K22
+	LATCbits.LATC7 = !LATCbits.LATC7;
+#endif
 	if ((data_in1 & 0xf0) == CMD_ADC_GO) { // Found a Master command
-	    channel=data_in1& 0x0f;
+	    channel = data_in1 & 0x0f;
+#ifdef P25K22
+	    if (channel > 4) channel += 7; // skip missing channels
+	    if (channel > 14) channel = 0; // invalid to set to 0
+#endif
+#ifdef P8722
+	    if (channel > 11) channel = 0; // invalid so set to 0
 	    LATEbits.LATE2 = !LATEbits.LATE2;
+#endif
 	    if (!ADCON0bits.GO) {
 		ADCON0 = ((channel << 2) & 0b00111100) | (ADCON0 & 0b11000011);
 		ADC_DATA = FALSE;
@@ -163,12 +198,16 @@ void InterruptHandlerHigh(void)
 		SSP1BUF = 0;
 	    }
 	}
-	if (data_in1 == CMD_DUMMY) {
+	if (data_in1 == CMD_DUMMY_CFG) {
+#ifdef P8722
 	    LATEbits.LATE3 = !LATEbits.LATE3;
+#endif
 	    SSP1BUF = CMD_DUMMY; // Tell master  we are here
 	}
 	if (data_in1 == CMD_ADC_DATAL) {
+#ifdef P8722
 	    LATEbits.LATE4 = !LATEbits.LATE4;
+#endif
 	    if (!ADCON0bits.GO) {
 		SSP1BUF = (uint8_t) (adc_buffer[channel] >> 8); // stuff with upper 8 bits
 	    } else {
@@ -176,7 +215,9 @@ void InterruptHandlerHigh(void)
 	    }
 	}
 	if (data_in1 == CMD_ADC_DATAH) {
+#ifdef P8722
 	    LATEbits.LATE5 = !LATEbits.LATE5;
+#endif
 	    if (!ADCON0bits.GO) {
 		SSP1BUF = (uint8_t) adc_buffer[channel]; // stuff with lower 8 bits
 	    } else {
@@ -190,17 +231,23 @@ void InterruptHandlerHigh(void)
 	data_in2 = SSP2BUF;
 
 	if (REMOTE_DATA_DONE) {
-	    if (data_in2 == CMD_DUMMY) { // Crack the whip
+	    if ((data_in2 & 0b11000000) == CMD_DUMMY_CFG) { // Crack that whip
+#ifdef P8722
 		LATEbits.LATE3 = !LATEbits.LATE3;
+#endif
 	    }
 	} else {
 	    if (LOW_BITS) {
 		adc_data_recv = (uint8_t) data_in2;
 		LOW_BITS = FALSE;
+#ifdef P8722
 		LATEbits.LATE6 = !LATEbits.LATE6;
+#endif
 	    } else {
 		adc_data_recv += (uint16_t) ((uint16_t) data_in2 << 8);
+#ifdef P8722
 		LATEbits.LATE7 = !LATEbits.LATE7;
+#endif
 		REMOTE_DATA_DONE;
 	    }
 	}
@@ -218,6 +265,8 @@ void wdtdelay(unsigned long delay)
 	ClrWdt(); // reset the WDT timer
     };
 }
+
+#ifdef P8722
 
 void DelayFor18TCY(void)
 {
@@ -293,13 +342,14 @@ void init_lcd(void)
 	lcd18 = 24;
     }
 }
+#endif
 
 void adc_conv_delay(void)
 {
     int16_t i, j, k = 0;
 
     for (i = 0; i < 1; i++) {
-	for (j = 0; j < 10; j++) {
+	for (j = 0; j < 20; j++) {
 	}
     }
 }
@@ -320,19 +370,52 @@ uint8_t spi_data_recd(void)
 
 void main(void) /* SPI Master/Slave loopback */
 {
-    int16_t i, j, k = 0;
+    int16_t i, j, k = 0, num_ai_chan = 0;
     uint8_t junk;
+
+#ifdef P8722
     TRISB = 0xff; // external interrupts for later
-    LATE = 0xff;
-    LATJ = 0xff;
     TRISE = 0;
     TRISF = 0;
     TRISJ = 0;
     TRISH = LOW; // mpuled and LCD
+    LATE = 0xff;
+    LATJ = 0xff;
     LATH = 0xff;
+    ADCON1 = 0x03; // adc [0..11] enable
+#endif
+#ifdef P25K22
+    TRISCbits.TRISC7 = 0; // Diag led
+    TRISAbits.TRISA6 = 0; // clock out
+
+    TRISBbits.TRISB1 = 0; // SSP2 pins
+    TRISBbits.TRISB2 = 1;
+    TRISBbits.TRISB3 = 0;
+
+    TRISCbits.TRISC3 = 1; // SSP1 pins
+    TRISCbits.TRISC4 = 1;
+    TRISCbits.TRISC5 = 0;
+
 
     /* ADC channels setup */
-    ADCON1 = 0x03; // adc [0..11] enable
+    TRISAbits.TRISA0 = HIGH; // an0
+    TRISAbits.TRISA1 = HIGH; // an1
+    TRISAbits.TRISA2 = HIGH; // an2
+    TRISAbits.TRISA3 = HIGH; // an3
+    TRISAbits.TRISA5 = HIGH; // an4
+    TRISBbits.TRISB4 = HIGH; // an11
+    TRISBbits.TRISB0 = HIGH; // SS2, don't use for analog
+    TRISBbits.TRISB5 = HIGH; // an13 
+    TRISCbits.TRISC2 = HIGH; // an14
+
+    ANSELA = 0b00101111; // analog bit enables
+    ANSELB = 0b00110000; // analog bit enables
+    ANSELC = 0b00000100; // analog bit enables
+    VREFCON0 = 0b11100000; // ADC voltage ref 2.048 volts
+    OpenADC(ADC_FOSC_RC & ADC_RIGHT_JUST & ADC_20_TAD, ADC_CH0 & ADC_INT_ON, ADC_REF_FVR_BUF & ADC_REF_VDD_VSS); // open ADC channel for current and voltage readings
+#endif
+
+#ifdef P8722
     TRISAbits.TRISA0 = HIGH; // an0
     TRISAbits.TRISA1 = HIGH; // an1
     TRISAbits.TRISA2 = HIGH; // an2
@@ -345,14 +428,17 @@ void main(void) /* SPI Master/Slave loopback */
     TRISFbits.TRISF4 = HIGH; // an9
     TRISFbits.TRISF5 = HIGH; // an10
     TRISFbits.TRISF6 = HIGH; // an11
+
     OpenADC(ADC_FOSC_RC & ADC_RIGHT_JUST & ADC_20_TAD, ADC_CH0 & ADC_REF_VDD_VSS & ADC_INT_ON, ADC_12ANA); // open ADC channel for current and voltage readings
+#endif
+
     PIE1bits.ADIE = HIGH; // the ADC interrupt enable bit
     IPR1bits.ADIP = HIGH; // ADC use high pri
 
     OpenSPI1(SLV_SSOFF, MODE_11, SMPMID); // Must be SMPMID in slave mode, Port C 4,5,3
     OpenSPI2(SPI_FOSC_64, MODE_11, SMPMID); // Must be SMPMID in slave mode, Port D 4,5,6
-    SSP1BUF = CMD_DUMMY;
-    SSP2BUF = CMD_DUMMY;
+    SSP1BUF = CMD_DUMMY_CFG;
+    SSP2BUF = CMD_DUMMY_CFG;
 
     PIR1bits.SSP1IF = 0;
     PIE1bits.SSP1IE = 1;
@@ -361,6 +447,7 @@ void main(void) /* SPI Master/Slave loopback */
     INTCONbits.PEIE = 1;
     INTCONbits.GIE = 1;
 
+#ifdef P8722
     init_lcd();
     strncpypgm2ram(bootstr2, build_time, LCD_W);
     LCD_VC_puts(VC0, DS0, YES);
@@ -371,6 +458,8 @@ void main(void) /* SPI Master/Slave loopback */
 	for (j = 0; j < 100; j++) {
 	}
     }
+#endif
+
     SSP1CON1bits.WCOL = SSP2CON1bits.WCOL = SSP1CON1bits.SSPOV = SSP2CON1bits.SSPOV = 0;
 
     while (1) {
@@ -378,8 +467,9 @@ void main(void) /* SPI Master/Slave loopback */
 	clear_spi_data_flag();
 	LOW_BITS = TRUE;
 	REMOTE_DATA_DONE = TRUE;
-	SSP2BUF = (CMD_ADC_GO&0b11111110)|(junk&0b00000001); // Master sends data
-	if (spi_data_recd() && (data_in2 == CMD_DUMMY)) {
+	SSP2BUF = (CMD_ADC_GO & 0b11111110) | (junk & 0b00000001); // Master sends data
+	if (spi_data_recd() && ((data_in2 & 0xf0) == CMD_DUMMY_CFG)) {
+	    num_ai_chan = data_in2 & 0x0f;
 	    REMOTE_DATA_DONE = FALSE;
 	    REMOTE_LINK = TRUE;
 	    adc_conv_delay();
@@ -387,18 +477,19 @@ void main(void) /* SPI Master/Slave loopback */
 	    SSP2BUF = CMD_ADC_DATAL; // Master sends cmd;
 	    spi_data_recd();
 	    clear_spi_data_flag();
-	    SSP2BUF = CMD_DUMMY; // Master sends DUMMY data;
+	    SSP2BUF = CMD_DUMMY_CFG; // Master sends DUMMY data;
 	    spi_data_recd();
 	    REMOTE_DATA_DONE = TRUE;
 	} else {
 	    REMOTE_LINK = FALSE;
-	    SSP2BUF = CMD_DUMMY; // Master sends DUMMY data;
+	    SSP2BUF = CMD_DUMMY_CFG; // Master sends DUMMY data;
+	    num_ai_chan = 0;
 	}
 
-
-
 	if (SSP1CON1bits.WCOL || SSP2CON1bits.WCOL || SSP1CON1bits.SSPOV || SSP2CON1bits.SSPOV) { // check for overruns/collisions
+#ifdef P8722
 	    LATHbits.LATH0 = !LATHbits.LATH0;
+#endif
 	    SSP1CON1bits.WCOL = SSP2CON1bits.WCOL = SSP1CON1bits.SSPOV = SSP2CON1bits.SSPOV = 0;
 	    adc_error_count = adc_count - adc_error_count;
 	}
@@ -410,16 +501,17 @@ void main(void) /* SPI Master/Slave loopback */
 	    for (j = 0; j < 1; j++) {
 	    }
 	}
+#ifdef P8722
 	if ((((k++) % 5000) == 0) || !REMOTE_LINK) {
 	    if (REMOTE_LINK) {
 		sprintf(bootstr2,
-			"The SPI Link is UP         "
-			);
+			"SPI UP %i Chan       ",
+			num_ai_chan);
 		LCD_VC_puts(VC0, DS0, YES);
 	    } else {
 		sprintf(bootstr2,
-			"The SPI Link is DOWN         "
-			);
+			"SPI DOWN %i Chan       ",
+			num_ai_chan);
 		LCD_VC_puts(VC0, DS0, YES);
 	    }
 	    if (ADC_DATA) {
@@ -439,9 +531,10 @@ void main(void) /* SPI Master/Slave loopback */
 	    LCD_VC_puts(VC0, DS2, YES);
 	    sprintf(bootstr2,
 		    "D %u, 0 %u 1 %u     ",
-		    adc_data_recv, adc_buffer[0],adc_buffer[1]);
+		    adc_data_recv, adc_buffer[0], adc_buffer[1]);
 	    LCD_VC_puts(VC0, DS3, YES);
 	}
+#endif
     };
 
 }
